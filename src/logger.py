bcdeb53ca386d39a939fb7c1ec03b644a1c4d1b3
Mihail <md@mihails-mbp.home>
2020-09-19 23:02:47 +0100

    updated queries

--- src/logger.py
+++ src/logger.py
@@ -2,27 +2,29 @@ import logging
 import datetime
 import os
 
-if not os.path.exists('logs'):
-    os.makedirs('logs')
+logs_folder='logs'
+if not os.path.exists(logs_folder):
+    os.makedirs(logs_folder)
 
 # Create a custom logger
 logger = logging.getLogger(__name__)
 
-# commenting out this line will activate logging
+# Setting global logging level
 logger.setLevel(logging.WARNING)
 
-# Create handlers
-dt=str(datetime.date.today()).replace('-','')
-f_handler = logging.FileHandler(f'logs/q_logs_{dt}.log')
-f_handler.setLevel(level=logging.DEBUG)
-c_handler = logging.StreamHandler()
-c_handler.setLevel(level=logging.DEBUG)
-# Create formatters and add it to handlers
-f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(funcName)s:%(lineno)d - %(message)s')
-f_handler.setFormatter(f_format)
-c_handler.setFormatter(f_format)
+date=str(datetime.date.today()).replace('-','')
+# Initialize handlers
+file_hndl = logging.FileHandler(f'{logs_folder}/q_logs_{date}.log')
+cli_hndl = logging.StreamHandler()
+# Set logging level
+file_hndl.setLevel(level=logging.DEBUG)
+cli_hndl.setLevel(level=logging.DEBUG)
+# Add formatters to handlers
+logger_text_format = logging.Formatter('%(asctime)s --- %(name)s --- %(levelname)s --- %(funcName)s:%(lineno)d --- %(message)s')
+file_hndl.setFormatter(logger_text_format)
+cli_hndl.setFormatter(logger_text_format)
 
 # Add handlers to the logger
-logger.addHandler(f_handler)
-logger.addHandler(c_handler)
+logger.addHandler(file_hndl)
+logger.addHandler(cli_hndl)
 
